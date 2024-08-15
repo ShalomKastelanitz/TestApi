03ef3684d486985f17831d1de20819c49788ef1b
This User <This User@DESKTOP-L9S4R74>
2024-08-15 17:54:49 +0300

    Add project files.

---
+++ TestApi/Controllers/TodosController.cs
@@ -0,0 +1,19 @@
+﻿
+//public class TodosController : Controller
+//{
+//    private readonly HttpClient _httpClient;
+
+//    public TodosController()
+//    {
+//        _httpClient = new HttpClient();
+//    }
+
+//    public async Task<IActionResult> Index()
+//    {
+//        string apiUrl = "https://dummyjson.com/todos";
+//        var response = await _httpClient.GetStringAsync(apiUrl);
+
+//        // הצגת ה-JSON הגולמי ישירות ב-View
+//        return Content(response, "application/json");
+//    }
+//}
---
+++ TestApi/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string TodoText { get; set; }
+        public bool Completed { get; set; }
+    }
+}
